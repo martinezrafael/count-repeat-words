2ff2ba962b158740245a7777a580f8a65b26f0df
martinezrafael <rafaelmartinez.contato@gmail.com>
2026-08-12 13:29:38 -0300

    Primeira implelemntação da captura de palavras

--- src/index.js
+++ src/index.js
@@ -4,7 +4,25 @@ const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 
 fs.readFile(link, 'utf-8', (err, data) => {
-    console.log(data);
+    verificaPalavrasDuplucadas(data);
 })
 
-console.log(link)
+// Criar um array com as palavras
+// Contar as ocorrências de cada palavra
+// Montar um objeto com as palavras e suas respectivas contagens
+
+function verificaPalavrasDuplucadas(data){
+
+    // o método split() divide uma string em um array de substrings, usando um separador especificado. No caso, o separador é um espaço em branco, o que significa que a string será dividida em palavras.
+    // O método split faz a divisão procurando um padrão dentro da string, e quando encontra esse padrão, ele cria uma nova substring a partir do ponto onde o padrão foi encontrado. O separador é removido da string original e não é incluído nas substrings resultantes.
+
+    const listaDePalavras = data.split(' '); // o array listaDePalavras conterá todas as palavras do texto, separadas por espaços em branco
+
+    const resultado = {}; // o objeto resultado será usado para armazenar as palavras e suas contagens
+
+    listaDePalavras.forEach(palavra => {
+        resultado[palavra] = (resultado[palavra] || 0) + 1; // se a palavra já existir no objeto resultado, incrementa sua contagem em 1; caso contrário, inicializa a contagem como 1
+    })
+
+    console.log(resultado); // exibe o objeto resultado no console, mostrando as palavras e suas contagens
+}
