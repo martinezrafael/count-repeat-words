b399c041324fea22f123a1f6fb597e01c3461cf9
martinezrafael <rafaelmartinez.contato@gmail.com>
2026-08-13 15:24:52 -0300

    Usando o flatMap

--- src/index.js
+++ src/index.js
@@ -13,14 +13,20 @@ fs.readFile(link, 'utf-8', (err, data) => {
 // Montar um objeto com as palavras e suas respectivas contagens
 
 
+
 function quebraEmParagrafos(data){
     const paragrafos = data.toLowerCase().split('\n'); // o método toLowerCase() converte todos os caracteres da string para minúsculas, garantindo que a contagem de palavras seja case-insensitive. O método split() divide a string em um array de substrings, usando o caractere de nova linha '\n' como separador. Isso significa que cada parágrafo do texto será um elemento do array paragrafos.
     
-    const contagem = paragrafos.map(paragrafo => { // o método map() cria um novo array com os resultados da chamada de uma função para cada elemento do array original. No caso, a função verificaPalavrasDuplucadas é chamada para cada parágrafo do array paragrafos, e o resultado é armazenado no array contagem.
-       return verificaPalavrasDuplucadas(paragrafo); 
+    const contagem = paragrafos.flatMap(paragrafo => {
+        if(!paragrafo) return [];
+        return verificaPalavrasDuplucadas(paragrafo);
     })
 
     console.log(contagem);
+}
+
+function limpaPalavras(palavra){
+    return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, ""); // o método replace() substitui todas as ocorrências de um padrão na string por um novo valor. No caso, o padrão é uma expressão regular que corresponde a todos os caracteres de pontuação e símbolos especificados, e o novo valor é uma string vazia, o que significa que esses caracteres serão removidos da palavra.
 }
 
 
@@ -34,7 +40,10 @@ function verificaPalavrasDuplucadas(data){
     const resultado = {}; // o objeto resultado será usado para armazenar as palavras e suas contagens
 
     listaDePalavras.forEach(palavra => {
-        resultado[palavra] = (resultado[palavra] || 0) + 1; // se a palavra já existir no objeto resultado, incrementa sua contagem em 1; caso contrário, inicializa a contagem como 1
+        if(palavra.length >= 3){
+            const palavraLimpa = limpaPalavras(palavra); // a função limpaPalavras é chamada para remover caracteres de pontuação e símbolos da palavra, garantindo que apenas palavras "limpas" sejam contadas
+            resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1; // se a palavra já existir no objeto resultado, incrementa sua contagem em 1; caso contrário, inicializa a contagem como 1
+        }
     })
 
     return resultado; // retorna o objeto resultado, que contém as palavras e suas respectivas contagens
